fb392907780e3c6367e8648d63c51d98c2d148f2
Lucas Araujo <araujo.l4k@hotmail.com>
2025-12-19 14:06:20 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/test/java/Novo/NovoModel.java
+++ src/test/java/Novo/NovoModel.java
@@ -1,18 +1,26 @@
 package Novo;
 
+
 import Missoes.MissoesModel;
 import jakarta.persistence.Entity;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 //Ele transforma uma classe comum em uma entidade no BD
 @Entity
 @Table(name = "tb_cadastro")	
+@NoArgsConstructor // automaticamente o lombok ele gera construtores controtor vazio
+@AllArgsConstructor // Ele gera automaticamento os construtores com parâmetros
+@Data //Ele vai criar automaticamente os getters e setters
 public class NovoModel {
 	
 	@Id
@@ -28,37 +36,9 @@ public class NovoModel {
 	
 	//@ManyToOne um ninja ter uma unica missao
 	@ManyToOne
+	@JoinColumn(name = "missoes_id") // Foreing Key ou cahve estrangeira
 	private MissoesModel missoes;
 	
-	
-	public NovoModel() {
-		
-	}
-	
-	public NovoModel(String nome, String email, int idade) {
-		super();
-		this.nome = nome;
-		this.email = email;
-		this.idade = idade;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
 	
 	
 	}
